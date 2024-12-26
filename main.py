10072dc4dc64050f5ee3f79885452352e5a75b18
Trading Desktop <khalilayari.jr@gmail.com>
2024-12-26 14:11:41 +0100

    more

--- main.py
+++ main.py
@@ -329,7 +329,7 @@ async def add_CIKs(tickers):  # This takes the dictionary and adds the CIKs to i
         results = await asyncio.gather(*tasks)
         print(f'Time to get all the CIKs {t.time() - start } s')
         for ticker in tickers:
-            print(ticker)
+            print(ticker['ticker'], str(ticker['gain']) + '%', ticker['link'])
         return tickers
 
 
